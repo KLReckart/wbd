6154d2d69e66d8536b791b8b8eeb7fc05d2a0f20
Kristi <Kristi@KLR-PC>
2016-09-11 11:33:20 -0500

    2nd commit

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -1,12 +1,44 @@
 class Angle():
     def __init__(self):
         #self.angle = ...       set to 0 degrees 0 minutes
+        self.degree = 0
+        self.mins = 0.0
         pass
+    
+    
+    #KLR's made function
+    # Purpose: return True if the string is a valid value, else returns False
+    def checkAngleString(self, angleString):
+        result = True
+        # does angleString have only 1 'd'
+        
+        # does angleString have a digit before 'd'
+        
+        # does angleString have a digit after 'd'
+        
+        
+        return result
     
     def setDegrees(self, degrees):
+        #default to zero if no numerical value 
+        
+        #if degrees is negative, convert to positive
+        
+        #set angleValue
+        
         pass
     
-    def setDegreesAndMinutes(self, degrees):
+    def setDegreesAndMinutes(self, angleString):
+        #if the input is valid, set angleValue
+        if self.checkAngleString(angleString) == True:
+            
+            
+            pass
+            
+            
+        
+        
+        
         pass
     
     def add(self, angle):
@@ -19,7 +51,19 @@ class Angle():
         pass
     
     def getString(self):
-        pass
+        #get the self.mins (up to the first decimal point)
+        formatMins = "{0:.1f}".format(self.mins)
+        result = "" + str(self.degree) + "d" + formatMins
+        
+        return result
     
     def getDegrees(self):
-        pass
+        #divide the minutes (up to the first digit behind decimal) by 60 to convert to degrees
+        minsToDegree = self.mins / 60.0
+        #add the minsToDegree to the degree value
+        result = self.degree + minsToDegree
+        #make sure the result is formatted correctly 
+        result = format(result, '0.1f')
+        #change the  result from string back to a float
+        result = float(result)
+        return result
---
+++ SoftwareProcess/Navigation/test/__init__.py
@@ -0,0 +1,22 @@
+import Navigation.prod.Angle as A
+
+#def main():
+    
+myAngle = A.Angle()
+    
+print("test get string: " + myAngle.getString() + " -> should be 0d0.0")
+print("test get degree: " + str(myAngle.getDegrees()) + " -> should be 0.0")
+    
+    #pass
+
+#def testAngleInstance():
+    
+    #pass
+
+#def testgetString(self, angleIN):
+    
+    #pass
+
+#def testgetDegree():
+    
+    #pass
