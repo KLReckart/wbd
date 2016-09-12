0b31bb324b98b6197e44f6d229daa2adff190776
Kristi <Kristi@KLR-PC>
2016-09-11 21:28:36 -0500

    commit 2

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -1,8 +1,8 @@
 class Angle():
     def __init__(self):
         #self.angle = ...       set to 0 degrees 0 minutes
-        self.degree = 0
-        self.mins = 0.0
+        self.angleInDegree = 0
+        #self.mins = 0.0
         pass
     
     
@@ -18,52 +18,126 @@ class Angle():
         
         
         return result
+    #KLR's made function
+    # Purpose: return minutes of an enter degree
+    def getMinsOfDegree(self, degreeIN):
+        result = (degreeIN - int(degreeIN)) * 60
+        #print("mins: " + str(degreeIN) + "->" + str(result))
+        return result
     
-    def setDegrees(self, degrees):
+    def setDegrees(self, degrees=0):
         #default to zero if no numerical value 
         
-        #if degrees is negative, convert to positive
+        #raise an error if the degrees value is neither an int nor float
+        if(not(isinstance(degrees, int)) and not(isinstance(degrees, float))):
+            raise ValueError("Angle.setDegrees:  the value entered for 'degrees' needs to be an integer or float")
+        else:        
+            #if degrees is negative, convert to positive
+            while degrees < 0 :
+                degrees = degrees + 360
+                pass
+            #if degree is .G.R. 360, subtract until .L.E. 360
+            while degrees > 360 :
+                degrees = degrees - 360
+                pass
+            
+            #set angleValue
+            # calculate the degree value
+            self.angleInDegree = degrees
         
-        #set angleValue
+            pass
         
         pass
     
     def setDegreesAndMinutes(self, angleString):
         #if the input is valid, set angleValue
         if self.checkAngleString(angleString) == True:
+            splitString = angleString.split('d')
             
+            #get num1
+            num1 = int(splitString[0])
+            #print "num1: " + str(num1)
+            #get num2
+            num2 = float(splitString[1])
+            #print "num2: " + str(num2)
+            
+            #get degrees (num1 modulo 360)
+            DM_degree = num1 % 360
+            #get mins (num2 modulo 60)
+            DM_min = num2 % 60
+            
+            #convert the degrees and mins to degree decimal
+            # divide mins by 60 and then add to degrees
+            self.angleInDegree = (DM_min / 60) + DM_degree
             
             pass
             
-            
-        
-        
         
         pass
     
-    def add(self, angle):
-        pass
+    def add(self, angleIN):
+        #add the two degrees
+        sumVal = self.angleInDegree + angleIN.angleInDegree
+        #if the result is Less Than 0, add 360 until .G.E. 0
+        while sumVal < 0 :
+            sumVal = sumVal + 360
+            pass
+        #if the result is Greater Than360, subtract until .L.E. 360
+        while sumVal > 360 :
+            sumVal = sumVal - 360
+            pass
+        #save the result to self.angleInDegree
+        self.angleInDegree = sumVal
+        #return the formatted result (format = first digit after decimal)
+        return format(sumVal, '0.1f')
+        
     
-    def subtract(self, angle):
-        pass
+    def subtract(self, angleIN):
+        #subtract the two degrees
+        diffVal = self.angleInDegree - angleIN.angleInDegree
+        #if the result is Less Than 0, add 360 until .G.E. 0
+        while diffVal < 0 :
+            diffVal = diffVal + 360
+            pass
+        #if the result is Greater Than360, subtract until .L.E. 360
+        while diffVal > 360 :
+            diffVal = diffVal - 360
+            pass
+        #save the result to self.angleInDegree
+        self.angleInDegree = diffVal
+        #return the formatted result (format = first digit after decimal)
+        return format(diffVal, '0.1f')
     
-    def compare(self, angle):
-        pass
+    def compare(self, angleIN):
+        #return -1 if self.angleInDegree < angleIN.angleInDegree, +1 if self.angleInDegree > angleIN.angleInDegree,
+        # 0 if self.angleInDegree = angleIN..angleInDegree
+        
+        #since the function that sets the degree angle ensure a value that is greater than 0 and less than 360,
+        # there is no need to ensure this range here
+        
+        if self.angleInDegree < angleIN.angleInDegree:
+            result = -1
+        elif self.angleInDegree > angleIN.angleInDegree:
+            result = +1
+        else:
+            result = 0
+            pass
+        
+        return result
     
     def getString(self):
-        #get the self.mins (up to the first decimal point)
-        formatMins = "{0:.1f}".format(self.mins)
-        result = "" + str(self.degree) + "d" + formatMins
+        #get the mins (up to the first decimal point)
+        mins = self.getMinsOfDegree(self.angleInDegree)
+
+        mins = format(mins, '0.1f')
+        result = str(int(self.angleInDegree)) + "d" + str(mins)
         
         return result
     
     def getDegrees(self):
-        #divide the minutes (up to the first digit behind decimal) by 60 to convert to degrees
-        minsToDegree = self.mins / 60.0
-        #add the minsToDegree to the degree value
-        result = self.degree + minsToDegree
         #make sure the result is formatted correctly 
-        result = format(result, '0.1f')
+        result = format(self.angleInDegree, '0.1f')
         #change the  result from string back to a float
         result = float(result)
+        
         return result
--- SoftwareProcess/Navigation/test/__init__.py
+++ SoftwareProcess/Navigation/test/__init__.py
@@ -6,7 +6,61 @@ myAngle = A.Angle()
     
 print("test get string: " + myAngle.getString() + " -> should be 0d0.0")
 print("test get degree: " + str(myAngle.getDegrees()) + " -> should be 0.0")
-    
+print("")
+
+# test getMinsOfDegree
+print("test getMinsOfDegree: " + str(myAngle.getMinsOfDegree(1.5)) + " -> should be 30")
+print("test getMinsOfDegree: " + str(myAngle.getMinsOfDegree(.1)) + " -> should be 6.0")
+print("")
+
+# test setDegrees
+print("myAngle.setDegrees()")
+myAngle.setDegrees()
+print("test setDegrees: " + str(myAngle.getDegrees()) + " -> should be zero")
+print("test setDegrees: " + str(myAngle.getString()) + " -> should be 0d0.0")
+print("myAngle.setDegrees(60)")
+myAngle.setDegrees(60)
+print("test setDegress: " + str(myAngle.getDegrees()) + " -> should be 60")
+print("test setDegrees: " + str(myAngle.getString()) + " -> should be 60d0.0")
+print("myAngle.setDegrees(400.5)")
+myAngle.setDegrees(400.5)
+print("test setDegrees: " + str(myAngle.getDegrees()) + " -> should be 40.5")
+print("test setDegrees: " + str(myAngle.getString()) + " -> should be 40d30.0")
+print("")
+
+# test setDegreesAndMinutes
+print("myAngle.setDegreesAndMinutes('0d0.0')")
+myAngle.setDegreesAndMinutes("0d0.0")
+print("test setDegreesAndMinutes: " + str(myAngle.getDegrees()) + " -> should be zero")
+print("test setDegreesAndMinutes: " + str(myAngle.getString()) + " -> should be 0d0.0")
+ 
+myAngle.setDegreesAndMinutes("60d0.0")
+print("test setDegrees: " + str(myAngle.getDegrees()) + " -> should be 60")
+print("test setDegrees: " + str(myAngle.getString()) + " -> should be 60d0.0")
+
+myAngle.setDegreesAndMinutes("75d1")
+print("test setDegreesAndMinutes: " + str(myAngle.getDegrees()) + " -> should be 75.?")
+print("test setDegreesAndMinutes: " + str(myAngle.getString()) + " -> should be 75d1.0")
+print(myAngle.angleInDegree)
+
+myAngle.setDegreesAndMinutes("-60d1")
+print("test setDegreesAndMinutes: " + str(myAngle.getDegrees()) + " -> should be 300.?")
+print("test setDegreesAndMinutes: " + str(myAngle.getString()) + " -> should be 300d1.0")
+print(myAngle.angleInDegree)
+print("")
+
+# test add()
+myAngle2 = A.Angle()
+print("myAngle2 initial degrees: " + str(myAngle2.getDegrees()) + " -> should be 0.0")
+print("myAngle2 initial string: " + str(myAngle2.getString()) + " -> should be 0d0.0")
+print("add myAngle to myAngle2")
+print(myAngle2.add(myAngle))
+print("myAngle2 final degrees: " + str(myAngle2.getDegrees()) + " -> should be 300.?")
+print("myAngle2 final string: " + str(myAngle2.getString()) + " -> should be 300d1.0")
+
+#create sad path tests
+
+
     #pass
 
 #def testAngleInstance():
