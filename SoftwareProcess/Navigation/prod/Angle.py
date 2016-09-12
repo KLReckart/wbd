6ccaa83b46488039dae5ebf9e0461dde8613aa2e
Kristi <Kristi@KLR-PC>
2016-09-11 22:59:36 -0500

    commit 3

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -5,17 +5,54 @@ class Angle():
         #self.mins = 0.0
         pass
     
+    #KLR's made function
+    # Purpose: return the entered value as an integer or float if possible
+    def num(self, valueIN):
+        try:
+            return int(valueIN)
+        except ValueError:
+            try:
+                return float(valueIN)
+            except ValueError:
+                return False
+            
+        
+        return True
+    
     
     #KLR's made function
     # Purpose: return True if the string is a valid value, else returns False
     def checkAngleString(self, angleString):
         result = True
         # does angleString have only 1 'd'
-        
-        # does angleString have a digit before 'd'
-        
-        # does angleString have a digit after 'd'
-        
+        countD = angleString.count("d")
+        if countD > 1 :
+            result = False
+            raise ValueError("Angle.checkAngleString:  the angleString has too many 'd's")
+        elif countD < 1 :
+            result = False
+            raise ValueError("Angle.checkAngleString:  the angleString lacks a 'd'")
+        else :
+        #b/c the next few checks are dependent of the 'd' being in the string, these checks need to be done if countD = 1
+            splitStringList = angleString.split("d")
+            # does angleString have a digit before 'd'
+            if(not(isinstance(self.num(splitStringList[0]), int))):
+                result = False
+                raise ValueError("Angle.checkAngleString:  the angleString must have an integer value before 'd'")
+                pass
+            # does angleString have a digit after 'd'
+            if(not(isinstance(self.num(splitStringList[1]), int)) and not(isinstance(self.num(splitStringList[1]), float))):
+                result = False
+                raise ValueError("Angle.checkAngleString:  the angleString must have an integer or float value after 'd'")
+            else:
+                #check that this int of float is positive, it needs to be
+                if self.num(splitStringList[1]) < 0 :
+                    result = False
+                    raise ValueError("Angle.checkAngleString:  the angleString must have a positive value after 'd'")
+                    pass
+                
+                pass
+            pass
         
         return result
     #KLR's made function
@@ -76,53 +113,70 @@ class Angle():
         pass
     
     def add(self, angleIN):
-        #add the two degrees
-        sumVal = self.angleInDegree + angleIN.angleInDegree
-        #if the result is Less Than 0, add 360 until .G.E. 0
-        while sumVal < 0 :
-            sumVal = sumVal + 360
+        #raise an error if the angleIN value is not an instance of class Angle
+        if(not(isinstance(angleIN, Angle)) and not(isinstance(angleIN, Angle))):
+            raise ValueError("Angle.add:  the value entered for 'angleIN' needs to be an instance of Angle")
+        else: 
+        
+            #add the two degrees
+            sumVal = self.angleInDegree + angleIN.angleInDegree
+            #if the result is Less Than 0, add 360 until .G.E. 0
+            while sumVal < 0 :
+                sumVal = sumVal + 360
+                pass
+            #if the result is Greater Than360, subtract until .L.E. 360
+            while sumVal > 360 :
+                sumVal = sumVal - 360
+                pass
+            #save the result to self.angleInDegree
+            self.angleInDegree = sumVal
+            #return the formatted result (format = first digit after decimal)
             pass
-        #if the result is Greater Than360, subtract until .L.E. 360
-        while sumVal > 360 :
-            sumVal = sumVal - 360
-            pass
-        #save the result to self.angleInDegree
-        self.angleInDegree = sumVal
-        #return the formatted result (format = first digit after decimal)
         return format(sumVal, '0.1f')
         
     
     def subtract(self, angleIN):
-        #subtract the two degrees
-        diffVal = self.angleInDegree - angleIN.angleInDegree
-        #if the result is Less Than 0, add 360 until .G.E. 0
-        while diffVal < 0 :
-            diffVal = diffVal + 360
+        #raise an error if the angleIN value is not an instance of class Angle
+        if(not(isinstance(angleIN, Angle)) and not(isinstance(angleIN, Angle))):
+            raise ValueError("Angle.subtract:  the value entered for 'angleIN' needs to be an instance of Angle")
+        else: 
+        
+            #subtract the two degrees
+            diffVal = self.angleInDegree - angleIN.angleInDegree
+            #if the result is Less Than 0, add 360 until .G.E. 0
+            while diffVal < 0 :
+                diffVal = diffVal + 360
+                pass
+            #if the result is Greater Than360, subtract until .L.E. 360
+            while diffVal > 360 :
+                diffVal = diffVal - 360
+                pass
+            #save the result to self.angleInDegree
+            self.angleInDegree = diffVal
+            #return the formatted result (format = first digit after decimal)
             pass
-        #if the result is Greater Than360, subtract until .L.E. 360
-        while diffVal > 360 :
-            diffVal = diffVal - 360
-            pass
-        #save the result to self.angleInDegree
-        self.angleInDegree = diffVal
-        #return the formatted result (format = first digit after decimal)
         return format(diffVal, '0.1f')
     
     def compare(self, angleIN):
-        #return -1 if self.angleInDegree < angleIN.angleInDegree, +1 if self.angleInDegree > angleIN.angleInDegree,
-        # 0 if self.angleInDegree = angleIN..angleInDegree
-        
-        #since the function that sets the degree angle ensure a value that is greater than 0 and less than 360,
-        # there is no need to ensure this range here
-        
-        if self.angleInDegree < angleIN.angleInDegree:
-            result = -1
-        elif self.angleInDegree > angleIN.angleInDegree:
-            result = +1
+        #raise an error if the angleIN value is not an instance of class Angle
+        if(not(isinstance(angleIN, Angle)) and not(isinstance(angleIN, Angle))):
+            raise ValueError("Angle.compare:  the value entered for 'angleIN' needs to be an instance of Angle")
         else:
-            result = 0
-            pass
         
+            #return -1 if self.angleInDegree < angleIN.angleInDegree, +1 if self.angleInDegree > angleIN.angleInDegree,
+            # 0 if self.angleInDegree = angleIN..angleInDegree
+        
+            #since the function that sets the degree angle ensure a value that is greater than 0 and less than 360,
+            # there is no need to ensure this range here
+        
+            if self.angleInDegree < angleIN.angleInDegree:
+                result = -1
+            elif self.angleInDegree > angleIN.angleInDegree:
+                result = +1
+            else:
+                result = 0
+                pass
+            pass
         return result
     
     def getString(self):
--- SoftwareProcess/Navigation/test/__init__.py
+++ SoftwareProcess/Navigation/test/__init__.py
@@ -57,12 +57,94 @@ print("add myAngle to myAngle2")
 print(myAngle2.add(myAngle))
 print("myAngle2 final degrees: " + str(myAngle2.getDegrees()) + " -> should be 300.?")
 print("myAngle2 final string: " + str(myAngle2.getString()) + " -> should be 300d1.0")
+print("")
+
+#test subtract()
+myAngle3 = A.Angle()
+print("myAngle3 initial degrees: " + str(myAngle3.getDegrees()) + " -> should be 0.0")
+print("myAngle3 initial string: " + str(myAngle3.getString()) + " -> should be 0d0.0")
+print("subtract myAngle from myAngle3")
+print(myAngle3.subtract(myAngle))
+print("myAngle2 final degrees: " + str(myAngle3.getDegrees()) + " -> should be 60.?")
+print("myAngle2 final string: " + str(myAngle3.getString()) + " -> should be 59d59.0")
+print("")
+
+#test compare()
+print("compare myAngle2 to myAngle3")
+print(str(myAngle2.compare(myAngle3)) + " -> should be +1")
+print("compare myAngle3 to myAngle2")
+print(str(myAngle3.compare(myAngle2)) + " -> should be -1")
+print("compare myAngle and myAngge2")
+print(str(myAngle.compare(myAngle2)) + " -> should be 0")
+print("")
 
 #create sad path tests
 
+#test add on sad path
+print("test add on sad path")
+try:
+    print(str(myAngle3.add(100)))
+except ValueError as e:
+    print(e)
 
+#test subtract on sad path
+print("test subtract on sad path")
+try:
+    print(str(myAngle3.subtract(100)))
+except ValueError as e:
+    print(e)
+
+#test subtract on sad path
+print("test compare on sad path")
+try:
+    print(str(myAngle3.compare(100)))
+except ValueError as e:
+    print(e)
+    
+#test num
+try:
+    print("test num")
+    print(myAngle3.num("5"))
+    print(myAngle3.num("5.5"))
+    print(myAngle3.num("abc"))
+except:
+    print("exception raised")
+    pass
+
+print("")
+
+#test checkAngleString
+try:
+    print(str(myAngle.checkAngleString("1d0")) + " -> should be True")
+    print(str(myAngle.checkAngleString("1d0.4")) + " -> should be True")
+    print(str(myAngle.checkAngleString("ad0")) + " -> should be False")
+
+except ValueError as e:
+    print(e)
+
+try:
+    print(str(myAngle.checkAngleString("1d-2")) + " -> should be False")
+except ValueError as e:
+    print(e)
+    
+try:
+    print(str(myAngle.checkAngleString("10")) + " -> should be False")
+except ValueError as e:
+    print(e)
+    
+try:
+    print(str(myAngle.checkAngleString("1dd0")) + " -> should be False")
+except ValueError as e:
+    print(e)
+
+
+try:
+    print("setDegreesAndMinutes('80')")
+    print(myAngle.setDegreesAndMinutes("80"))
+except ValueError as e:
+    print(e)
     #pass
-
+    
 #def testAngleInstance():
     
     #pass
