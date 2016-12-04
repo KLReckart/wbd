f4f67e978eda8144f965240503caff054d9328e6
Kristi <Kristi@KLR-PC>
2016-12-04 14:51:46 -0600

    CA05 wrote test test100_05testLogData, and did a readlight test

--- SoftwareProcess/Navigation/test/CA05_test.py
+++ SoftwareProcess/Navigation/test/CA05_test.py
@@ -39,7 +39,10 @@ class FixTest_CA05(unittest.TestCase):
                     os.remove(self.RANDOM_LOG_FILE)  
             except:
                 pass
-            
+    
+    def cleanup(self):
+        if(os.path.isfile("log.txt")):
+            os.remove("log.txt") 
 #---------- 
 
 # Fix.getSightings
@@ -67,6 +70,7 @@ class FixTest_CA05(unittest.TestCase):
         
         # I have decide to come back to this test last, after performing TDD on all other tests since the other tests are
         #  less complex
+        self.cleanup()
         pass
         
     def test100_02testDefaultLongitudeInput(self):
@@ -88,6 +92,7 @@ class FixTest_CA05(unittest.TestCase):
         expected = ("0d0.0")
         self.assertEquals(result, expected, str(result) + " != " + str(expected))
         
+        self.cleanup()
         pass
         
     def test100_03testDefaultLatitudeInput(self):
@@ -109,6 +114,8 @@ class FixTest_CA05(unittest.TestCase):
         result = thisFix.assumedLat
         expected = ("0d0.0")
         self.assertEquals(result, expected, str(result) + " != " + str(expected))
+        
+        #self.cleanup()
         pass
     
     def test100_04testDefaultInputs(self):
@@ -136,10 +143,58 @@ class FixTest_CA05(unittest.TestCase):
         pass
     
     def test100_05testLogData(self):
+        #create Fix object
+        thisFix = F.Fix()
         
+        #set aries, sighting, and star files
+        ariesFile = self.ariesFileName
+        sightFile = self.sightFileName
+        starFile = self.starFileName
+        file01 = thisFix.setSightingFile(sightFile)
+        file02 = thisFix.setAriesFile(ariesFile)
+        file03 = thisFix.setStarFile(starFile)
+        
+        #call getSighitngs
+        assumedLat = "N27d59.5"
+        assumedLong = "85d33.4"
+        getResult = thisFix.getSightings(assumedLat, assumedLong)
+        
+        #check that the file was created
+        file01Name = "log.txt"
+        self.assertTrue(os.path.isfile(file01Name), "file01 does not exist")
+        #if file was created, check that the first line is correct
+        
+        
+        #open the log file for reading
+        logFile = open("log.txt", "r")
+        #get the 5th and 7th lines as a string
+        line5 = ""
+        line7 = ""
+        
+        position = logFile.seek(0, 0)
+        currentLine = 1
+        for aLine in logFile:
+            print (aLine + "\n")
+            if (currentLine == 5):
+                line5 = aLine
+            #print "line#: " + str(currentLine)
+            #print "line2: " + line2
+            if (currentLine == 7):
+                line7 = aLine
+            currentLine = currentLine + 1
+        
+        #close file
+        logFile.close()
+        #print("secondTolastLine: " + secondTolastLine)
+        #set expectedString
+        expectedString5 = ("LOG: 2016-10-01 10:01:10-06:00\t" + "Pollux\t" + "2017-04-14\t"+ "23:50:14\t" + "15d01.5\t" 
+                             + "27d59.1\t" + "84d33.4\t" + "N27d59.5\t" + "85d33.4\t" + "292d44.6\t" + "174\n")
+        expectedString7 = ("LOG: 2016-10-01 10:01:11-06:00\t" + "Approximate latitude:\t" + "N29d6.8\t" 
+                           + "Approximate longitude:\t" + "82d52.9\n")
+        #check 5th line
+        self.assertEquals(line5, expectedString5, (line5 + " ! =" + expectedString5))
         #check 7th line
-        
-        #check 8th line
+        self.assertEquals(line7, expectedString7, (line7 + " != " + expectedString7))
         
         pass
     
